f80959ec31fe2858665e133037bb289002ea496b
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2025-09-10 16:59:45 +0100

    feat(addresses): add AuthModule to AddressesModule imports

--- src/modules/addresses/addresses.module.ts
+++ src/modules/addresses/addresses.module.ts
@@ -5,9 +5,10 @@ import { ServiceZone } from './service-zone.entity';
 import { AddressesService } from './addresses.service';
 import { AddressesController } from './addresses.controller';
 import { User } from '../users/user.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Address, ServiceZone, User])],
+  imports: [TypeOrmModule.forFeature([Address, ServiceZone, User]), AuthModule],
   controllers: [AddressesController],
   providers: [AddressesService],
   exports: [TypeOrmModule, AddressesService],
